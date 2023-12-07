e2c8dc4a2af53703db0f2cea3eca0eb6a8506ee8
junwon_lee <wjdrbs307@naver.com>
2023-12-07 14:35:29 +0900

    12 07 chapter10,11,12

--- src/index.js
+++ src/index.js
@@ -11,9 +11,12 @@ import NotificationList from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
 import ConfirmButton from './chapter_08/ConfirmButton';
 import LandingPage from './chapter_09/LandingPage';
+import AttendanceBook from './chapter_10/AttendanceBook';
+import SignUp from './chapter_11/SignUp';
+import Calculator from './chapter_12/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<LandingPage/>);
+root.render(<Calculator/>);
 
 reportWebVitals();
 
